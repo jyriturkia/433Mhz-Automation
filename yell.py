27401ed69fd0221f8be4fea9573fcf2f0cb55706
jyriturkia <46651721+jyriturkia@users.noreply.github.com>
2019-01-22 22:24:26 +0200

    On and off pulses to be the same. Long off is now word separator. Moved signal init to be static part of loop. No need to include it in 3-base signal code anymore.

--- yell.py
+++ yell.py
@@ -3,21 +3,20 @@ import sys
 import RPi.GPIO as GPIO
 
 # On/off from remote
-on = 'I102020111112011111210112021021020'
-off = 'I102020111112011111210112021111020'
+on = '102020111112011111210112021021020'
+off = '102020111112011111210112021111020'
 # working on/off signals
-#on = 'I102020111112011111210112021021020'
-#on = 'I102020111112011111210112021021021'
-#on = 'I102020111112011111210112021021022'
-#off = 'I102020111112011111210112021111020'
-#off = 'I102020111112011111210112021111021'
-#off = 'I102020111112011111210112021111022'
+#on = '102020111112011111210112021021020'
+#on = '102020111112011111210112021021021'
+#on = '102020111112011111210112021021022'
+#off = '102020111112011111210112021111020'
+#off = '102020111112011111210112021111021'
+#off = '102020111112011111210112021111022'
 
 
 init_delay = 0.002676
-on_delay = 0.000265
-short_off_delay = 0.00027 # this might be same as on_delay
-long_off_delay = 0.001295
+pulse_delay = 0.000265
+word_separator = 0.001295
 repeate_delay = 0.010255
 
 NUM_ATTEMPTS = 5
@@ -29,39 +28,38 @@ def transmit_code(code):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(TRANSMIT_PIN, GPIO.OUT)
     for t in range(NUM_ATTEMPTS):
+        GPIO.output(TRANSMIT_PIN, 1)
+        time.sleep(pulse_delay)
+        GPIO.output(TRANSMIT_PIN, 0)
+        time.sleep(init_delay)
         for i in code:
-            if i == 'I':
+            if i == '0':
                 GPIO.output(TRANSMIT_PIN, 1)
-                time.sleep(on_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(init_delay)
-            elif i == '0':
-                GPIO.output(TRANSMIT_PIN, 1)
-                time.sleep(on_delay)
-                GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(long_off_delay)
+                time.sleep(word_separator)
             elif i == '1':
                 GPIO.output(TRANSMIT_PIN, 1)
-                time.sleep(on_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(short_off_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 1)
-                time.sleep(on_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(long_off_delay)
+                time.sleep(word_separator)
             elif i == '2':
                 GPIO.output(TRANSMIT_PIN, 1)
-                time.sleep(on_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(short_off_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 1)
-                time.sleep(on_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(short_off_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 1)
-                time.sleep(on_delay)
+                time.sleep(pulse_delay)
                 GPIO.output(TRANSMIT_PIN, 0)
-                time.sleep(long_off_delay)
+                time.sleep(word_separator)
             else:
                 continue
         GPIO.output(TRANSMIT_PIN, 0)
